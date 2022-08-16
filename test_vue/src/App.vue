62682d519e18ef19324f9f2a75eccc86f9d7d4ba
gcm <1453400328@qq.com>
2022-08-16 11:14:19 +0800

    默认插槽、具名插槽、作用域插槽

--- test_vue/src/App.vue
+++ test_vue/src/App.vue
@@ -5,21 +5,77 @@
     <!-- <School @myEvent="getSchoolName"></School> -->
     <!-- 自定义事件第二种写法1 -->
     <School ref="school"></School>
+
+    <Category title="默认插槽">
+      <ul>
+        <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+      </ul>
+    </Category>
+
+    <Category title="具名插槽1">
+      <h4 slot="slot1">hhhhh</h4>
+      <ol slot="slot1">
+        <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+      </ol>
+    </Category>
+
+    <Category title="具名插槽1">
+      <template slot="slot1">
+        <h4>hhhhh</h4>
+        <ol>
+          <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+        </ol>
+      </template>
+    </Category>
+
+    <Category title="具名插槽1">
+      <template v-slot:slot1>
+        <h4>hhhhh</h4>
+        <ol>
+          <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+        </ol>
+      </template>
+    </Category>
+
+    <Category title="作用域插槽1">
+      <template scope="scopeData">
+        <h4>{{ scopeData }}</h4>
+        <ol>
+          <li v-for="(item, index) in scopeData.movies" :key="index">
+            {{ item }}
+          </li>
+        </ol>
+      </template>
+    </Category>
+
+    <Category title="作用域插槽2">
+      <template scope="scopeData" slot="slot2">
+        <h4>{{ scopeData }}</h4>
+        <ol>
+          <li v-for="(item, index) in scopeData.movies" :key="index">
+            {{ item }}
+          </li>
+        </ol>
+      </template>
+    </Category>
   </div>
 </template>
 <script>
 import Student from "./components/Student.vue";
 import School from "./components/School.vue";
+import Category from "./components/Category.vue";
 export default {
   name: "App",
   data() {
     return {
       name: "gg",
+      games: ["lol", "tft", "pubg"],
     };
   },
   components: {
     Student,
     School,
+    Category,
   },
   methods: {
     getStudentName(name) {
@@ -31,7 +87,7 @@ export default {
   },
   mounted() {
     // 自定义事件第二种写法2
-    this.$refs.school.$on("myEvent",this.getSchoolName)
+    this.$refs.school.$on("myEvent", this.getSchoolName);
   },
 };
 </script>
---
+++ test_vue/src/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <br />
+    <slot>默认插槽的默认值</slot><br />
+    <slot name="slot1">具名插槽slot1默认值</slot><br />
+    <slot :movies="movies">作用域插槽的默认值</slot><br />
+    <slot :movies="movies" name="slot2">作用域插槽slot2的默认值</slot><br />
+  </div>
+</template>
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+  data() {
+    return {
+      movies: ["古惑仔", "无间道", "逃学威龙", "赌圣"],
+    };
+  },
+};
+</script>
+<style scoped>
+.category {
+  background-color: pink;
+}
+</style>
